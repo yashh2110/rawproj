fc19aca3d57bf32f3214203f49dbfe32631d3d52
Yashwanth Muddana <77020506+yashh2110@users.noreply.github.com>
2021-04-10 17:25:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var right=document.getElementById("right");
-var left=document.getElementById("left");
-var rec_list = document.getElementsByClassName("rec_list");
+const right=document.getElementById("right");
+const left=document.getElementById("left");
+const rec_list = document.getElementsByClassName("rec_list");
 right.addEventListener("click",()=>{
-  var pos = rec_list[0].offsetLeft;
+  const pos = rec_list[0].offsetLeft;
   if(pos >-440){
-  var moveleft= pos-225;
-  var moveleft = moveleft.toString();
+  const moveleft= pos-225;
+  const moveleft = moveleft.toString();
   document.getElementById("rec_list").style.left= moveleft+"px";
 }
 });
 left.addEventListener("click",()=>{
-  var pos = rec_list[0].offsetLeft;
+  const pos = rec_list[0].offsetLeft;
   if(pos!=0){
-    var moveleft= pos+225;
-    var moveleft = moveleft.toString();
+    const moveleft= pos+225;
+    const moveleft = moveleft.toString();
     document.getElementById("rec_list").style.left= moveleft+"px";
   }
 })
